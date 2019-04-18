b0fa4e9573c4804632b25a3b661ca4a6f95e5f10
aldinsabb <aldinisa.rahma@ui.ac.id>
2019-04-18 19:28:23 +0700

    Modul 7

---
+++ src/ItemStatus.java
@@ -0,0 +1,14 @@
+
+/**
+ * Enumeration class itemStatus - write a description of the enum class here
+ *
+ * @author (your name here)
+ * @version (version number or date here)
+ */
+public enum ItemStatus
+{
+    New,
+    Second,
+    Refurbished,
+    Sold;
+}
---
+++ src/Location.java
@@ -0,0 +1,96 @@
+
+/**
+ * Kelas digunakan sebagai informasi lokasi
+ *
+ * @author Aldinisa Rahma Sabillah
+ * @version 27/3/2019
+ */
+public class Location
+{
+    private String province;
+    private String description;
+    private String city;
+
+    /**
+     * Constructor for objects of class Location
+     */
+    public Location(String city, String province, String description)
+    {
+        this.city = city;
+        this.province = province;
+        this.description = description;
+    }
+
+    /**
+     * Method untuk mengembalikan nama provinsi
+     *
+     * @return    nama provinsi
+     */
+    public String getProvince()
+    {
+        return province;
+    }
+    
+    /**
+     * Method untuk mengembalikan nama kota
+     *
+     * @return    nama kota
+     */
+    public String getCity()
+    {
+        return city;
+    }
+    
+    /**
+     * Method untuk mengembalikan deskripsi kota
+     *
+     * @return    deskripsi kota
+     */
+    public String getDescription()
+    {
+        return description;
+    }
+    
+    /**
+     * Method untuk mengubah nama provinsi
+     *
+     * @param  province  nama provinsi
+     */
+    public void setProvince(String province)
+    {
+        this.province = province;
+    }
+    
+    /**
+     * Method untuk mengubah nama kota
+     *
+     * @param  city  nama kota
+     */
+    public void setCity(String city)
+    {
+        this.city = city;
+    }
+    
+    /**
+     * Method untuk mengubah deskripsi kota
+     *
+     * @param  description  deskripsi kota
+     */
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+    
+    /**
+     * Method untuk menampilkan nama provinsi
+     *
+     */
+    public String toString()
+    {
+        System.out.println("==========LOCATION==========");
+        System.out.println("Province: " + province);
+        System.out.println("City: " + city);
+        System.out.println("Description: " + description);
+        return "";
+    }
+}
